272e3fa47ebcce02828b38c0a9a6daa3e6c3618d
Lahcen97 <lahcen.mohamed1997@gmail.com>
2021-09-28 11:23:49 +0200

    fix(.gitignore)

--- job-board-front/src/App.js
+++ job-board-front/src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-
 import Navbar from './components/Navbar/Navbar';
 
 
--- job-board-front/src/components/Navbar/Navbar.tsx
+++ job-board-front/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './Navbar.css';
+import Navv from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+
 
 const Navbar = () => (
   <div className="Navbar">
-    Navbar Component
+    <Navv bg="primary" variant="dark">
+      <Container>
+        <Navv.Brand href="#home">Navbar</Navv.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Container>
+    </Navv>
   </div>
+
 );
 
 export default Navbar;
