80f094aa86c13e60a1bf67e48ab46a4cc5794861
Lahcen97 <lahcen.mohamed1997@gmail.com>
2021-09-29 13:51:54 +0200

    (feat(index.js):added register rout)

--- job-board-back/index.js
+++ job-board-back/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const db = require('./config')
 const app = express()
-const crypto = require('crypto');
 const PORT = process.env.PORT || 5000
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const someOtherPlaintextPassword = 'not_bacon';
 
 app.use(express.json())
 
@@ -20,14 +23,41 @@ app.get('/adverts', (req, res) => {
 
 app.post('/login', (req, res) => {
     console.log(req.body.email)
-   if (!req.body || !req.body.password || !req.body.email) {
-       res.status(406).send('field_missing')
-   }
-   db.query(`SELECT * FROM people WHERE email="${req.body.email}"`, (err, res) => {
-       if (err) throw err
-       console.log(res)
-   })
+    if (!req.body || !req.body.password || !req.body.email) {
+        res.status(406).send('field_missing')
+    }
+
+    db.query(`SELECT * FROM people WHERE email="${req.body.email}"`, (err, res) => {
+        if (err) throw err
+        console.log(res)
+    })
 })
+
+app.get('/register', (req, response) => {
+
+
+    if (!req.body || !req.body.firstName || !req.body.lastName || !req.body.password || !req.body.email) {
+        response.status(406).send('field_missing')
+    }
+    const ligne = db.query(`select email from  people where email="${req.body.email}"`, (err, res) => {
+        if (err) throw err
+        if (res.length == 0) {
+            const myPlaintextPassword = req.body.password;
+            bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
+                db.query(`insert into people (first_name , name, password_ ,email)  values ("${req.body.firstName}","${req.body.lastName}","${hash}","${req.body.email}")`, (err, res) => {
+                    if (err) throw err
+                    response.status(200).send('success');
+                })
+
+            });
+
+        } else {
+            response.status(406).send('email_exist');
+        }
+    })
+})
+
+
 
 
 app.listen(PORT, () => {
