7df674a24307b365061847e54bd4e9e2e64daa30
LyannB <lyann.bourlon@epitech.eu>
2021-09-29 13:25:13 +0200

    fix(index.js, AdvertPage.tsx): Allowed cors

--- job-board-back/index.js
+++ job-board-back/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const db = require('./config')
 const app = express()
 const crypto = require('crypto');
+const cors = require('cors')
 const PORT = process.env.PORT || 5000
+app.use(cors())
 
 app.use(express.json())
 
--- job-board-front/src/components/AdvertPage/AdvertPage.tsx
+++ job-board-front/src/components/AdvertPage/AdvertPage.tsx
@@ -1,18 +1,23 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import classes from './AdvertPage.module.css';
 import axios from "axios"
 
 const AdvertPage = () => {
   useEffect(() => {
     console.log("useEffect")
-    axios.get('http://localhost:5000/adverts')
-    .then(res => {
-      console.log(res)
-    }).catch(err => console.error(err))
+    axios.get('http://localhost:5000/adverts', {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+      }
+    })
+      .then(res => {
+        console.log(res)
+      }).catch(err => console.error(err))
   })
- return ( <div className={classes.AdvertPage}>
-  AdvertPage Component
-</div>)
+  return (<div className={classes.AdvertPage}>
+    AdvertPage Component
+  </div>)
 };
 
 export default AdvertPage;
