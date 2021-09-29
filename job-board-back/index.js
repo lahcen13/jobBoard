37a4904a8b1de0a7f28ae763f2fa647c1846e253
LyannB <lyann.bourlon@epitech.eu>
2021-09-29 11:14:49 +0200

    feat(back/login): added login route but still need to compare password (waiting for hash feature)

--- job-board-back/index.js
+++ job-board-back/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const db = require('./config')
 const app = express()
+const crypto = require('crypto');
 const PORT = process.env.PORT || 5000
 
+app.use(express.json())
 
 db.connect((err) => {
     if (err) throw err
@@ -14,6 +16,17 @@ app.get('/adverts', (req, res) => {
         if (err) throw err
         console.log(res)
     })
+})
+
+app.post('/login', (req, res) => {
+    console.log(req.body.email)
+   if (!req.body || !req.body.password || !req.body.email) {
+       res.status(406).send('field_missing')
+   }
+   db.query(`SELECT * FROM people WHERE email="${req.body.email}"`, (err, res) => {
+       if (err) throw err
+       console.log(res)
+   })
 })
 
 
