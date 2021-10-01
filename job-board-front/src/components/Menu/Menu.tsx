54e717e3874f4fd0b2e74dc36d082d05ec66d190
Lahcen97 <lahcen.mohamed1997@gmail.com>
2021-10-02 01:24:58 +0200

    feature(getUserToken): setup deconnexion

--- job-board-front/src/components/Menu/Menu.tsx
+++ job-board-front/src/components/Menu/Menu.tsx
@@ -3,11 +3,15 @@ import { propTypes } from 'react-bootstrap/esm/Image';
 import classes from './Menu.module.css';
 import { ReactComponent as Person } from '../../assets/svgs/person.svg'
 import { ReactComponent as Power } from '../../assets/svgs/power.svg'
+import { remove } from '../../functions/session'
 
 const Menu = (props: any) => {
   const menu = useRef<HTMLDivElement>(null)
   const [className, setClassName] = useState<any>(classes.isClose)
 
+  const onClick = () => {
+    remove();
+  }
   useEffect(() => {
     if (menu.current) {
       console.log('menu')
@@ -18,7 +22,7 @@ const Menu = (props: any) => {
   return (
     <div ref={menu} className={`${classes.menu} ${className}`}>
       <Person className={classes.icon} />
-      <Power className={classes.icon} />
+      <Power onClick={() => onClick()} className={classes.icon} />
     </div>
   )
 };
--- job-board-front/src/functions/session.tsx
+++ job-board-front/src/functions/session.tsx
@@ -1,26 +1,36 @@
+import { WindowSidebar } from 'react-bootstrap-icons'
 import expireDate from './expireDate'
 
 const set = (token: string, perm: boolean) => {
 
-    if (perm) {
-        const data: session = { token: token, expire: expireDate().toString() }
-      return  localStorage.setItem("session", JSON.stringify(data))
-    }
+  if (perm) {
     const data: session = { token: token, expire: expireDate().toString() }
-   return sessionStorage.setItem("session", JSON.stringify(data));
+    return localStorage.setItem("session", JSON.stringify(data))
+  }
+  const data: session = { token: token, expire: expireDate().toString() }
+  return sessionStorage.setItem("session", JSON.stringify(data));
 }
 
 
 const get = () => {
-    const perm = localStorage.getItem('session')
-    if (perm) return perm
-    return sessionStorage.getItem('session')
+  const perm = localStorage.getItem('session')
+  if (perm) return perm
+  return sessionStorage.getItem('session')
+}
+
+
+const remove = () => {
+
+  window.sessionStorage.removeItem('session')
+  window.localStorage.removeItem('session')
+  window.location.href = '/login';
+
 }
 
 interface session {
-    token: string,
-    expire: string,
-  }
+  token: string,
+  expire: string,
+}
 
 
-  export {set, get}
+export { set, get, remove }
