4f5d365c09d8d7c5a0fef6fea0e098dcf29f2887
LyannB <lyann.bourlon@epitech.eu>
2021-10-12 17:12:28 +0200

    fix(login.js)

--- job-board-back/middleware/handleToken.js
+++ job-board-back/middleware/handleToken.js
@@ -2,27 +2,27 @@ var jwt = require('jsonwebtoken');
 const { check, getPayload } = require('../functions/token');
 module.exports = (req, res, next, arr) => {
     if (arr.includes(req.path)) return next()
-const token = req.headers.authorization.split(' ')[1]
-console.log('TOOOOOKEN', token)
-const decoded = getPayload(token)
-
-if (decoded.role === 'user' && req.path.startsWith('/admin')) return res.status(401).send('admin_reserved')
-if(decoded.role === 'admin') {
-    const checked = check(token, decoded.role)
-    if (checked === 'no_permission') {
-       return res.status(401).send('bad_signature')
-    }else {
-       return next()
+    const token = req.headers.authorization.split(' ')[1]
+    console.log('TOOOOOKEN', token)
+    const decoded = getPayload(token)
+    if (decoded.role === 'user' && req.path.startsWith('/company')) return res.status(401).send('company_reserved')
+    if (decoded.role === 'user' || decoded.role === 'company' && req.path.startsWith('/admin')) return res.status(401).send('admin_reserved')
+    if (decoded.role === 'admin') {
+        const checked = check(token, decoded.role)
+        if (checked === 'no_permission') {
+            return res.status(401).send('bad_signature')
+        } else {
+            return next()
+        }
     }
-}
-if (decoded.role === 'user') {
-    const checked = check(token, decoded.role)
-    if (checked === 'no_permission') {
-        return res.status(401).send('bad_signature')
-    }else {
-       return next()
+    if (decoded.role === 'user') {
+        const checked = check(token, decoded.role)
+        if (checked === 'no_permission') {
+            return res.status(401).send('bad_signature')
+        } else {
+            return next()
+        }
     }
-}
 
-next()
+    next()
 }
