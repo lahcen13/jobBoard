e3ea08c759228886db4fce1e3393f22070168160
Lahcen97 <lahcen.mohamed1997@gmail.com>
2021-10-14 16:03:02 +0200

    fix css page acceuil

--- job-board-front/src/components/Visitor-page/Visitor-page.tsx
+++ job-board-front/src/components/Visitor-page/Visitor-page.tsx
@@ -8,21 +8,21 @@ import Footer from '../Footer/Footer'
 
 const Visitorpage = () => (
 
-  <div className="Visitor-page">
+  <div className={styles.VisitorPage}>
     <Navbar />
-    <div className={styles.main}>
-      <div className={styles.introSection}>
-        <p className={styles.intro}>Find your dream job among a list of 1000+ companies.</p>
-        <div className={styles.underline}></div>
-      </div>
-      <div className={styles.redirects}>
-        <div className={styles.redirectLogin}>
-          <input onClick={() => window.location.href = '/login'} className={styles.logButton} type='button' value="I'm looking for a company" />
-          <input onClick={() => window.location.href = '/company/login'} className={styles.logButton} type='button' value="I'm a company" />
-        </div>
-        <input onClick={() => window.location.href = '/adverts'} className={styles.button} type='button' value="See all advertisments" />
-      </div>
+
+    <div className={styles.introSection}>
+      <p className={styles.intro}>Find your dream job among a list of 1000+ companies.</p>
+      <div className={styles.underline}></div>
     </div>
+    <div className={styles.redirects}>
+      <div className={styles.redirectLogin}>
+        <input onClick={() => window.location.href = '/login'} className={styles.logButton} type='button' value="I'm looking for a company" />
+        <input onClick={() => window.location.href = '/company/login'} className={styles.logButton} type='button' value="I'm a company" />
+      </div>
+      <input onClick={() => window.location.href = '/adverts'} className={styles.button} type='button' value="See all advertisments" />
+    </div>
+
     <Footer />
   </div>
 );
